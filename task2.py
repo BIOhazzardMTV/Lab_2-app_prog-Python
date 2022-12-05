318b2b857e7afbbdc20756ca6361d98b7f225f1f
BIOhazzardMTV <fedorov200375@mail.ru>
2022-12-05 12:07:46 +0400

    Создание функции "get_csv"
    Функция "get_csv" создаёт csv-файл, в который записывает абсолютный, относительный пути к файлу, а также метку этого класса

--- task2.py
+++ task2.py
@@ -1,12 +1,30 @@
 from shutil import rmtree, copy
-from os import mkdir, listdir
-from os.path import isdir, abspath
+from os import mkdir, listdir, remove, chdir
+from os.path import isdir, abspath, relpath, isfile
+from csv import DictWriter
 
 
 def dataset_copy(class_label: str, destination: str):
     source = abspath(f"dataset/{class_label}")
     for i in listdir(source):
         copy(f"{source}\\{i}", f"{destination}\\{class_label}_{i}")
+
+
+def get_csv(destination: str):
+    chdir("dataset2")
+    if isfile("annotation.csv"):
+        remove("annotation.csv")
+    file = open("annotation.csv", "a+")
+    names = ["Absolute path", "Relative path", "Class label"]
+    csv_file = DictWriter(file, lineterminator="\r", fieldnames=names)
+    csv_file.writeheader()
+    for i in listdir(destination):
+        class_label = (i.split("_"))[0]
+        csv_file.writerow({
+            "Absolute path": f"{abspath(i)}",
+            "Relative path": f"{relpath(i,start='C:/Users/Professional/PycharmProjects/pythonProject')}",
+            "Class label": class_label
+        })
 
 
 if __name__ == "__main__":
