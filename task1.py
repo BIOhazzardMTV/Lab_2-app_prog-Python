5f366c94d39045f1b4d303e51d6f8b6b4df69df8
BIOhazzardMTV <fedorov200375@mail.ru>
2022-12-05 11:56:07 +0400

    Создание функции "get_csv"
    Функция "get_csv" принимает в качестве параметра открытый в формате "a+"(дозапись) csv-файл, в который затем записывает данные основного датасета

--- task1.py
+++ task1.py
@@ -1,6 +1,26 @@
-from os import chdir, path, remove
+import _csv
+from csv import DictWriter
+from os import chdir, remove, listdir
+from os.path import abspath, relpath, isfile
+
+
+def get_csv(file: '_csv.writer', class_label: str):
+    """Принимает открытый для дозаписи csv-файл и метку класса, записывает в csv-файл данные"""
+    chdir(class_label)
+    for i in listdir():
+        file.writerow({
+         "Absolute path": f"{abspath(i)}",
+         "Relative path": f"{relpath(i,start='C:/Users/Professional/PycharmProjects/pythonProject')}",
+         "Class label": class_label
+        })
+    chdir("..")
+
+
 if __name__ == "__main__":
     chdir('dataset')
-    if path.isfile("annotation.csv"):
+    if isfile("annotation.csv"):
         remove("annotation.csv")
     file_w = open("annotation.csv", "a+")
+    names = ["Absolute path", "Relative path", "Class label"]
+    csv_file = DictWriter(file_w, lineterminator="\r", fieldnames=names)
+    csv_file.writeheader()
