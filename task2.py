f4c21480ef5c564faa208ea3f14318fd1747e3de
BIOhazzardMTV <fedorov200375@mail.ru>
2022-12-05 12:03:05 +0400

    Создание функции "dataset_copy"
    Функция "dataset_copy" копирует все файлы из директории source в директорию destination, при этом переименовывая их таким образом, чтобы имена файлов содержали в себе метку класса и порядковый номер

--- task2.py
+++ task2.py
@@ -1,9 +1,20 @@
-from shutil import rmtree
-from os import mkdir
-from os.path import isdir
+from shutil import rmtree, copy
+from os import mkdir, listdir
+from os.path import isdir, abspath
+
+
+def dataset_copy(class_label: str, destination: str):
+    source = abspath(f"dataset/{class_label}")
+    for i in listdir(source):
+        copy(f"{source}\\{i}", f"{destination}\\{class_label}_{i}")
+
+
 if __name__ == "__main__":
     if not isdir("dataset2"):
         mkdir("dataset2")
     else:
         rmtree('dataset2')
         mkdir('dataset2')
+    dest = abspath('dataset2')
+    dataset_copy("cat", dest)
+    dataset_copy("dog", dest)
